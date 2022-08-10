71081c5807bb7ede4d63dae1b4cdcd5358ea5d3d
ocelo8421 <8ocelot@gmail.com>
2022-08-10 19:41:38 +0200

    20220810 desktop PC

--- src/main/java/com/artur/intakes/controller/MedicineController.java
+++ src/main/java/com/artur/intakes/controller/MedicineController.java
@@ -1,9 +1,6 @@
 package com.artur.intakes.controller;
 
 import com.artur.intakes.dto.DayOfWeekDTO;
-import com.artur.intakes.dto.MedicationIntakeDTO;
-import com.artur.intakes.dto.MedicineDTO;
-import com.artur.intakes.entity.DayOfWeek;
 import com.artur.intakes.service.MedicationIntakeService;
 import com.artur.intakes.service.MedicineService;
 import org.springframework.beans.factory.annotation.Autowired;
