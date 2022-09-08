7370434fda9c3e8e81bc5d1df3a0a76a294dd0b7
ocelo8421 <8ocelot@gmail.com>
2022-09-08 23:06:52 +0200

    able to make new connection between existing objects

--- src/main/java/com/artur/intakes/service/MedicineService.java
+++ src/main/java/com/artur/intakes/service/MedicineService.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MedicineService {
+    private static final Long NEW_MED_MARKER_ID = 99L;
 
     @Autowired
     private DayOfWeekService dayOfWeekService;
@@ -74,19 +76,18 @@ public class MedicineService {
         medicationIntakeRepository.deleteById(checkID);
     }
 
-//    public MedicationIntakeDTO createAndUpdateIntake(ConnectionDTO connection) {
+    //    public MedicationIntakeDTO createAndUpdateIntake(ConnectionDTO connection) {
     public void createAndUpdateIntake(ConnectionDTO connection) {
         MedicationIntake medicationIntake = new MedicationIntake();
-        medicationIntake.setId(connection.getId());
         DayOfWeek dayOfWeek = dayOfWeekService.retrieveDayOfWeek(connection);
-
         Date date = new Date();
         date.setId(connection.getDate());
         TimeOfDay timeOfDay = new TimeOfDay();
         timeOfDay.setId(connection.getTimeOfDay());
-//        Medicine medicine = new Medicine();
-//        medicine.setId(connection.getMedicine());
-        Medicine medicine = connection.getMedicine();
+
+        Long medicineID = retrieveMedicineID(connection.getMedicine());
+        Medicine medicine = new Medicine();
+        medicine.setId(medicineID);
 
         System.out.println("connection:"); // put -----------------------------
         System.out.println(connection);
@@ -96,11 +97,34 @@ public class MedicineService {
         medicationIntake.setDate(date);
         medicationIntake.setTimeOfDay(timeOfDay);
         medicationIntake.setMedicine(medicine);
+        medicationIntake.setId(connection.getId() - NEW_MED_MARKER_ID + medicineID);
+
         System.out.println(medicationIntake);
 
         MedicationIntake intake = medicationIntakeRepository.save(medicationIntake);
 //        return new MedicationIntakeDTO(intake);
     }
+
+    private Long retrieveMedicineID(Medicine medicine) {
+        Long medID = 99L;
+        String medName = medicine.getName();
+        System.out.println("got medicine:");
+        System.out.println(medicine);
+        for (Medicine aMedicine : medicineRepository.findAll()) {
+            //TODO ugly this so many if-branch
+            if (Objects.equals(aMedicine.getName(), medName)
+                    && aMedicine.getDose() == medicine.getDose()
+                    && aMedicine.getPieces() == medicine.getPieces()
+            ) {
+                System.out.println(aMedicine.getId());
+                System.out.println(aMedicine.getName());
+                medID = aMedicine.getId();
+            }
+        }
+        return medID;
+    }
+
+
 }
 
 
